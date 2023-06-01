43b0caac9a1869c816021667ba1fc11512838238
mnoor920 <mnoor92081@hotmail.com>
2023-06-01 14:04:46 +0500

    placeholder updated

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,12 +3,15 @@ import Layout from "../layout/layout"
 import Cards from '../components/cards'
 const Home = () => {
     const [data, setData] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
     useEffect(() => {
         const fetchData = async () => {
+            setIsLoading(true)
             try {
                 const response = await fetch('https://file.notion.so/f/s/24643894-e5c3-4c40-974a-52594f581e03/listings.json?id=f795dab6-14d4-48a9-9567-c72151d311a2&table=block&spaceId=f2ea7328-64a4-4f18-bacc-df6c9ac3d888&expirationTimestamp=1685614360897&signature=y6gaECGUjdM-q0HwDbbkB9Y8u5PGGy_ObUlQxFjpLo0&downloadName=listings.json');
                 const jsonData = await response.json();
                 setData(jsonData && jsonData.data);
+                setIsLoading(false)
             } catch (error) {
                 console.error('Error fetching data:', error);
             }
@@ -22,13 +25,22 @@ const Home = () => {
     return (
         <div>
             <Layout data={data}>
-                <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 sm:grid-cols-2 max-w-[1250px] m-auto gap-4 px-2">
-                    {data?.map((item) => {
-                        return (
-                            <Cards item={item?.info} />
-                        )
-                    })}
 
+                <div className="">
+                    {isLoading ? (
+                        <div className="placeholder-container">
+                            <div className="placeholder-box"></div>
+                        </div>
+                    ) : (
+                        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 sm:grid-cols-2 max-w-[1250px] m-auto gap-4 px-2">
+                            {data?.map((item) => {
+                                return (
+                                    <Cards item={item?.info} />
+                                )
+                            })}
+
+                        </div>
+                    )}
                 </div>
             </Layout>
         </div>
--- src/pages/searchPage.js
+++ src/pages/searchPage.js
@@ -9,6 +9,7 @@ import { useLocation } from 'react-router-dom';
 
 const SearchPage = () => {
     const [data, setData] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
     const location = useLocation();
     const searchParams = new URLSearchParams(location.search);
     const city = searchParams.get('city');
@@ -19,10 +20,12 @@ const SearchPage = () => {
     });
     useEffect(() => {
         const fetchData = async () => {
+            setIsLoading(true)
             try {
                 const response = await fetch('https://file.notion.so/f/s/24643894-e5c3-4c40-974a-52594f581e03/listings.json?id=f795dab6-14d4-48a9-9567-c72151d311a2&table=block&spaceId=f2ea7328-64a4-4f18-bacc-df6c9ac3d888&expirationTimestamp=1685614360897&signature=y6gaECGUjdM-q0HwDbbkB9Y8u5PGGy_ObUlQxFjpLo0&downloadName=listings.json');
                 const jsonData = await response.json();
                 setData(jsonData && jsonData.data);
+                setIsLoading(false)
             } catch (error) {
                 console.error('Error fetching data:', error);
             }
@@ -41,45 +44,56 @@ const SearchPage = () => {
     console.log("Searched Room", locations)
     return (
         <Layout data={data}>
-            <div className="grid grid-cols-1 md:grid-cols-5 max-w-[1250px] m-auto gap-4 px-2">
-                {searchedRoom.length ? (
-                    <>
-                        <div className="col-span-3">
-                            <div className="grid grid-cols-1 md:grid-cols-3 sm:grid-cols-2 max-w-[1250px] m-auto gap-4">
-                                {searchedRoom?.map((item) => {
-                                    return (
-                                        <Cards item={item?.info} />
-                                    )
-                                })}
 
-                            </div>
-                        </div>
-                        <div className="col-span-2">
-                            <div className="sticky top-0">
-                                {locations.length ? (
-                                    <MapContainer center={[locations[0]?.lat, locations[0]?.lng]} zoom={10} style={{ height: '100vh', width: '100%' }}>
-                                        <TileLayer
-                                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                                            attribution="Map data © OpenStreetMap contributors"
-                                        />
-                                        {locations?.map((location, index) => (
-                                            <Marker key={index} position={[location?.lat, location?.lng]} icon={customIcon} >
-                                                <Popup>{location?.city}</Popup>
-                                            </Marker>
-                                        ))}
-                                    </MapContainer>
-                                ) : ''}
+            <div className="">
+                {isLoading ? (
+                    <div className="placeholder-container">
+                        <div className="placeholder-box"></div>
+                    </div>
+                ) : (
+                    <div className="grid grid-cols-1 md:grid-cols-5 max-w-[1250px] m-auto gap-4 px-2">
+                        {searchedRoom.length ? (
+                            <>
+                                <div className="col-span-3">
+                                    <div className="grid grid-cols-1 md:grid-cols-3 sm:grid-cols-2 max-w-[1250px] m-auto gap-4">
+                                        {searchedRoom?.map((item) => {
+                                            return (
+                                                <Cards item={item?.info} />
+                                            )
+                                        })}
 
-                            </div>
-                        </div>
-                    </>
-                ) :
-                    <h2>
-                        Room Not Found
-                    </h2>
-                }
+                                    </div>
+                                </div>
+                                <div className="col-span-2">
+                                    <div className="sticky top-0">
+                                        {locations.length ? (
+                                            <MapContainer center={[locations[0]?.lat, locations[0]?.lng]} zoom={10} style={{ height: '100vh', width: '100%' }}>
+                                                <TileLayer
+                                                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                                                    attribution="Map data © OpenStreetMap contributors"
+                                                />
+                                                {locations?.map((location, index) => (
+                                                    <Marker key={index} position={[location?.lat, location?.lng]} icon={customIcon} >
+                                                        <Popup>{location?.city}</Popup>
+                                                    </Marker>
+                                                ))}
+                                            </MapContainer>
+                                        ) : ''}
 
+                                    </div>
+                                </div>
+                            </>
+                        ) :
+                            <h2>
+                                Room Not Found
+                            </h2>
+                        }
+
+                    </div>
+                )}
             </div>
+
+
         </Layout>
     )
 }
